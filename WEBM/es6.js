23af4223f02832a8cc620ade21557b99f9425230
Yufon <506184177@qq.com>
2021-02-02 15:52:50 +0800

    Update es6.js
    add removeTrack

--- WEBM/es6.js
+++ WEBM/es6.js
@@ -1,12 +1,18 @@
 /* eslint-disable class-methods-use-this */
-import Vue from 'vue';
+const recordVideo = Symbol('recordVideo');
+const stream = Symbol('stream');
+const recorder = Symbol('recorder');
+const recordVideoSrc = Symbol('stream');
 
 class WebmVideo {
-    constructor(recordVideo) {
-        this.recordVideo = recordVideo;
-        this.stream = null;
-        this.recorder = null;
-        this.recordVideoSrc = null;
+    constructor(recordVideoDom) {
+        // private
+        this[recordVideo] = recordVideoDom;
+        this[stream] = null;
+        this[recorder] = null;
+        this[recordVideoSrc] = null;
+        // public
+        this.videoFile = null;
     }
 
     static caniuse() {
@@ -14,63 +20,61 @@ class WebmVideo {
     }
 
     init() {
-        const height = this.recordVideo.clientHeight * window.devicePixelRatio;
-        Vue.prototype.$loading.show(true);
+        const height = this[recordVideo].clientHeight * window.devicePixelRatio;
         return navigator.mediaDevices.getUserMedia({
             audio: true,
             video: {
                 facingMode: 'user',
                 height
             }
-        }).then((stream) => {
-            Vue.prototype.$loading.show(false);
-            this.recordVideo.autoplay = true;
-            this.recordVideo.muted = true;
-            this.recordVideo.srcObject = stream;
-            this.stream = stream;
-            this.recorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
-        }).catch((err) => {
-            Vue.prototype.$toast.info(err);
-            return Promise.reject(err);
-        });
+        }).then((mediaStream) => {
+            this[recordVideo].autoplay = true;
+            this[recordVideo].muted = true;
+            this[recordVideo].srcObject = mediaStream;
+            this[stream] = mediaStream;
+            this[recorder] = new MediaRecorder(mediaStream, { mimeType: 'video/webm' });
+        }).catch(err => Promise.reject(err));
     }
 
     start() {
-        if (this.recorder.state === 'inactive') {
-            this.recorder.start();
+        if (this[recorder].state === 'inactive') {
+            this[recorder].start();
         }
     }
 
     stop() {
         return new Promise((resolve) => {
-            this.recorder.ondataavailable = (res) => {
+            this[recorder].ondataavailable = (res) => {
                 console.log(res);
-                const videoFile = new Blob([res.data], { type: 'video/webm' });
-                this.recordVideoSrc = URL.createObjectURL(videoFile);
-                this.recordVideo.srcObject = null;
-                this.recordVideo.autoplay = false;
-                this.recordVideo.muted = false;
-                this.recordVideo.src = this.recordVideoSrc;
-                this.recordVideo.controls = true;
+                this.videoFile = new Blob([res.data], { type: 'video/webm' });
+                this[recordVideoSrc] = URL.createObjectURL(this.videoFile);
+                this[recordVideo].srcObject = null;
+                this[recordVideo].autoplay = false;
+                this[recordVideo].muted = false;
+                this[recordVideo].src = this[recordVideoSrc];
+                this[recordVideo].controls = true;
 
-                return resolve(videoFile);
+                return resolve(this.videoFile);
             };
-            this.recorder.stop();
+            this[recorder].stop();
         });
     }
 
     rerecord() {
-        URL.revokeObjectURL(this.recordVideoSrc);
-        console.log(this.recordVideoSrc);
-        this.recordVideo.autoplay = true;
-        this.recordVideo.muted = true;
-        this.recordVideo.src = null;
-        this.recordVideo.controls = false;
-        this.recordVideo.srcObject = this.stream;
+        URL.revokeObjectURL(this[recordVideoSrc]);
+        this[recordVideo].autoplay = true;
+        this[recordVideo].muted = true;
+        this[recordVideo].src = null;
+        this[recordVideo].controls = false;
+        this[recordVideo].srcObject = this[stream];
     }
 
     destory() {
-        URL.revokeObjectURL(this.recordVideoSrc);
+        URL.revokeObjectURL(this[recordVideoSrc]);
+        this[stream].getTracks().forEach((track) => {
+            track.stop();
+            this[stream].removeTrack(track);
+        });
     }
 }
 
